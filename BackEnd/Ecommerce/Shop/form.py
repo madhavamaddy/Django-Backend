b067bcbed864a38b6741807db8fad97975db1d9e
Django <you@example.com>
2023-07-11 11:31:08 -0700

    first commit

---
+++ BackEnd/Ecommerce/Shop/form.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+class CustomUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter user name'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter user email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'enter user password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'enter user confrim password'}))
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
